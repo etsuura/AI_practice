3b74687e7da92cee6725c9c3e3f2332bc4fbb643
etsuura <etsuura@gmail.com>
2019-10-05 12:32:00 +0900

    add ex2.4

--- ex2.py
+++ ex2.py
@@ -1,12 +1,5 @@
 #人工知能概論練習課題
 
-graph = {
-"S" : list(("A" ,"B")),
-"A" : list(("S", "C", "D")),
-"B" : list(("S", "C")),
-"C" : list(("A", "B", "D")),
-"D" : list(("A", "C"))}
-
 def depthFirstSearch(graph, start):
     openlist = [start]
     closedlist = []     #step1
@@ -16,9 +9,12 @@ def depthFirstSearch(graph, start):
     while len(openlist) != 0:   #step2
         print(openlist, closedlist)
         vertex = openlist.pop(0)
-        closedlist.append(vertex)   #step3
-        path = graph[vertex]  #step4
-        for i in range(len(path)):
+        if (vertex == "G"):     #step3
+            closedlist.append(vertex)  # step4
+            break
+        closedlist.append(vertex)   #step4
+        path = graph[vertex]
+        for i in range(len(path)):  #step5
             if (path[i] in closedlist) == False and (path[i] in openlist) == False:
                 openlist.insert(0, path[i])
         path = []
@@ -35,7 +31,10 @@ def breadthFirstSearch(graph, start):
     while len(openlist) != 0:   #step2
         print(openlist, closedlist)
         vertex = openlist.pop(0)
-        closedlist.append(vertex)   #step3
+        if (vertex == "G"):     #step3
+            closedlist.append(vertex)  # step4
+            break
+        closedlist.append(vertex)
         path = graph[vertex]  #step4
         for i in range(len(path)):
             if (path[i] in closedlist) == False and (path[i] in openlist) == False:
@@ -45,10 +44,46 @@ def breadthFirstSearch(graph, start):
     print(openlist, closedlist)
     print("finish")
 
+def ex2_4(method):
+    graph = {
+        "S": list(("3")),
+        "1": list(("4")),
+        "2": list(("6")),
+        "3": list(("S", "4", "7")),
+        "4": list(("1", "3", "6")),
+        "5": list(("8")),
+        "6": list(("2", "4", "G")),
+        "7": list(("3", "8", "9")),
+        "8": list(("5", "7", "10")),
+        "9": list(("7")),
+        "10": list(("8")),
+        "G": list(("6"))}
+
+    print("ex2_4")
+
+    if (method == "dfs"):
+        depthFirstSearch(graph, "S")
+    elif(method == "bfs"):
+        breadthFirstSearch(graph, "S")
+    else:
+        print("else")
+
 def main():
+    graph = {
+        "S": list(("A", "B")),
+        "A": list(("S", "C", "D")),
+        "B": list(("S", "C")),
+        "C": list(("A", "B", "D")),
+        "D": list(("A", "C"))}
+
     depthFirstSearch(graph, "S")
+    print("__________________________________")
     breadthFirstSearch(graph, "S")
-    #print(path)
+    print("__________________________________")
+    ex2_4("dfs")
+    print("__________________________________")
+    ex2_4("bfs")
+    print("__________________________________")
 
 if __name__ == "__main__":
     main()
